842c7738141d918d201c94f5bbf7d9260f2ab1a9
Donelteeto <fadiyatolulope150@gmail.com>
2023-02-17 17:27:09 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,11 @@
 /**
  * main - this is a function to print
  *
- * Return; 0 if and when correct
+ * Return: 0 if and when correct
  */
 
 int main(void)
-
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
