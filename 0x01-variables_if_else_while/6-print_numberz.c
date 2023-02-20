ad1e443867460ca8cc965687bce7b5149ca22468
Donelteeto <fadiyatolulope150@gmail.com>
2023-02-20 15:17:10 -0800

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0 (as always)
+ */
+
+int main(void)
+{
+	int j;
+
+	for (j = 0 ; j < 10 ; j++)
+		putchar(j + '0');
+	putchar('\n');
+	return (0);
+}
