3840aa058a7d9d5fdd6ba41570939c53f1c18e6c
Donelteeto <fadiyatolulope150@gmail.com>
2023-02-20 15:53:39 -0800

    8-print_base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers of base 16
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int j;
+	char k;
+
+	for (j = 0 ; j < 10 ; j++)
+		putchar(j + '0');
+	for (k = 'a' ; k <= 'f' ; k++)
+		putchar(k);
+	putchar('\n');
+	return (0);
+}
