f9637e975511cb313eeebf22e0817353ee5acaa5
Donelteeto <fadiyatolulope150@gmail.com>
2023-02-23 13:29:25 -0800

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * add - function that adds two numbers
+ *
+ * @a: parameter
+ *
+ * @b: parameter 2
+ *
+ * Return: sum
+ */
+
+int add(int a, int b)
+{
+	int a;
+	int b;
+
+	sum = a + b;
+
+	return (sum);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * times_table - function that prints 9 times table from 0
+ *
+ * Return: 0
+ */
+
+void times_table(void)
+{
+
+}
