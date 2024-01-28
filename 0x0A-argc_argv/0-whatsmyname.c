d2545676945b4a32369ddad4ad1af6e546b86e4f
Donelteeto <fadiyatolulope150@gmail.com>
2024-01-28 13:20:04 -0800

    0x0A-argc_argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * main: this prints the program name
+ * main-this prints the program name
  * @argc: this prints the count of arguments passed to the program
  * @argv: this outputs the size of the argument
  * Return: 0 upon success
