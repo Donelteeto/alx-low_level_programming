178ec46743281cd0759e7e7c02326193f0e9dc87
Donelteeto <fadiyatolulope150@gmail.com>
2024-01-28 12:56:23 -0800

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main: this prints the program name
+ * @argc: this prints the count of arguments passed to the program
+ * @argv: this outputs the size of the argument
+ * Return: 0 upon success
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc
+		printf("%s\n", *argv);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+This is the README for Argc and Argv which are command line arguments to the main function
