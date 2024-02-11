55b2a87a3d90bd9714404e673424fc83f3d5974c
Donelteeto <fadiyatolulope150@gmail.com>
2024-02-10 17:05:24 -0800

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,9 +4,11 @@
 
 
 /*
- * create_array - function that creates an array of char
+ * create_array - function that creates an array of characters
  * @size: size of the memory to print
  * @c: address of char/memory to print
+ * This function dynamically allocates memory for an array of characters
+ * of size 'size' and initializes each element with the character 'c'.
  * Return: a pointer
  */
 
@@ -15,11 +17,15 @@ char *create_array(unsigned int size, char c)
 	char *ptrZ;
 	unsigned int b;
 
+	if (size == 0)
+	{
+		return (NULL);
+	}
 	ptrZ = (char *)malloc(size * sizeof(char));
 
 	if (ptrZ == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 	for (b = 0; b < size; b++)
 	{
